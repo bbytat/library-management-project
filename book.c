5682868a78d9bc3bfdde779048b386fe6b674fdb
bbytat <tatiana75013@icloud.com>
2022-10-22 13:16:34 +0200

    update

--- book.c
+++ book.c
@@ -2,10 +2,10 @@
 
 // on cree un menu demandant a l'utilisateur les renseignements pour trouver et obtenir le livre dans la bibliotheque
 
-void get()
+void getBook()
 {
     char c ;
-    printf("        - S to search in the library C\n");
+    printf("        - S to search in the library\n");
     printf("        - A to add a new book in the library\n");
     printf("        - D to delete a book from the library\n");
     printf("        - R to return into the main menu");
@@ -33,7 +33,7 @@ void show(t_all all)
 //on affiche les informations sur l'adherent par rapport au livre choisi
 void askBook(t_all all)
 {
-    Book member;
+    t_all member;
     printf("enter informations about the book : \n");
     printf(" - index :   ");
     scanf("%d", &(all.index));
